87029b739269518300eb3b459474fbae39f48e4f
AyushJoshi1403 <ayush.aj1403@gmail.com>
2026-08-24 23:54:12 +0530

    update

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const expenseForm = document.getElementById("expense-form")
     const expenseNameInput = document.getElementById("expense-name")
+    const expenseAmountInput = document.getElementById("expense-amount")
+    const expenseList = document.getElementById("expense-list")
 })
